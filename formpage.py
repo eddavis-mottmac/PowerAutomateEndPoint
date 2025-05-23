59871551045480216e24b4c8b2158cec602ac9c4
eddavis-mottmac <ed.davis@mottmac.com>
2025-05-23 14:21:12 +0100

    status code updated

--- formpage.py
+++ formpage.py
@@ -242,7 +242,7 @@ if st.button("Submit"):
        
         response = submitform(form_data)
         if response.status_code == 200:
-            st.success(f"{response.status_code}: Thank you, you have successfully submitted the form! You shall receieve a confirmation email shortly.")
+            ("Thank you, you have successfully submitted the form! You shall receieve a confirmation email shortly.")
         else:
             st.error(f"{response.status_code} error code, please contact systems administrator")
 
